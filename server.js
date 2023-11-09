564f1746a5a3777a5b28c5728488686f7c3277de
yhss1015 <dudgns8481@gmail.com>
2023-11-09 14:12:31 +0900

    초기 커밋

---
+++ server.js
@@ -0,0 +1,34 @@
+import express from "express"
+import mysql from 'mysql'
+
+const app = express()
+const port = 3010
+
+const db = mysql.createConnection({
+	host: 'svc.sel5.cloudtype.app',
+    port: 31772,
+	user: 'root',
+	password: 'mysql1234',
+	database: 'songdb',
+})
+
+db.connect()
+app.get('/', (req, res) => {
+  res.json({result: '스마트앱프로그래밍 백엔드'})
+})
+
+app.get('/song', (req, res) => {
+  const sql = "select * from song"
+  db.query(sql, (err,rows) => {
+    if (err) {
+      res.json({ result: 'error'})
+      return console.log(err)
+    }
+    res.json(rows)
+  })
+  
+})
+
+app.listen(port, () => {
+  console.log(`서버 실행됨 (port ${port})`)
+})
